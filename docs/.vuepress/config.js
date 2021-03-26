470db8ad2f7d89c93ee79e4ab5d6f0fbf6aecf9b
imwbh <wbh2031@163.com>
2021-03-27 02:33:48 +0800

    Update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,12 +9,12 @@ module.exports = {
           {text: '友链', link: ''}      
         ],
         sidebar: 'auto', // 侧边栏配置
-        lastUpdated: 'Last Updated', // string | boolean
+        lastUpdated: '最后更新于', // string | boolean
         // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
-        repo: 'imwbh/vuepress',
+        repo: 'imwbh/stm32',
         // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
         // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
-        repoLabel: '查看源码',
+        //repoLabel: 'Repo',
         // 以下为可选的编辑链接选项
         // 假如你的文档仓库和项目本身不在一个仓库：
         //docsRepo: 'vuejs/vuepress',
@@ -25,7 +25,7 @@ module.exports = {
         // 默认是 false, 设置为 true 来启用
         editLinks: true,
         // 默认为 "Edit this page"
-        editLinkText: '在Github编辑',
+        editLinkText: 'edit on github',
         smoothScroll: true,
     },
     locales: {
